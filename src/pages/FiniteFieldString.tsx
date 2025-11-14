8b4c6ccc35dfb574931096ff23562f323c82a667
Zazzik1 <mateuszbilicki34@gmail.com>
2025-11-14 20:13:39 +0100

    minor ui improvements, added support for multiline secrets

--- src/pages/FiniteFieldString.tsx
+++ src/pages/FiniteFieldString.tsx
@@ -1,17 +1,21 @@
-import { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import { getSecretFromSharesStr, makeSharesStr } from '../util';
 import { Link } from 'react-router-dom';
 
 const _PRIME = 2 ** 13 - 1; // https://en.wikipedia.org/wiki/Mersenne_prime
+const initialSecret = `hello world
+test 123
+super secret`;
 
 const FiniteFieldString = () => {
-    const [secret, setSecret] = useState('hello world');
+    const [secret, setSecret] = useState(initialSecret);
     const [numberOfShares, setNumberOfShares] = useState(3);
     const [numberOfSharesToReconstruct, setNumberOfSharesToReconstruct] =
         useState(3);
     const [sharesToReconstruct, setSharesToReconstruct] = useState<string[]>(
         [],
     );
+    const [error, setError] = useState<string | null>(null);
     const [reconstructedSecret, setReconstructedSecret] = useState('');
     const shares = useMemo(
         () => makeSharesStr(secret, numberOfShares, _PRIME),
@@ -19,12 +23,12 @@ const FiniteFieldString = () => {
     );
     const reconstructSecret = useCallback(() => {
         setReconstructedSecret('');
+        setError(null);
         try {
             const secret = getSecretFromSharesStr(sharesToReconstruct);
             setReconstructedSecret(secret);
         } catch (err) {
-            setReconstructedSecret(`Error: ${err}`);
-            console.error(err);
+            if (err instanceof Error) setError(err.message);
         }
     }, [sharesToReconstruct]);
     const loadShares = useCallback(() => {
@@ -32,7 +36,17 @@ const FiniteFieldString = () => {
         setSharesToReconstruct(shares);
     }, [shares, numberOfShares]);
     return (
-        <div>
+        <div style={{ padding: '8px' }}>
+            <div
+                style={{
+                    textAlign: 'center',
+                    fontWeight: '600',
+                    color: 'rgb(158, 190, 128)',
+                }}
+            >
+                Shamir's Secret Sharing - Finite field arithmetic - share &
+                reconstruct tool
+            </div>
             <div style={{ display: 'flex', flexDirection: 'column' }}>
                 <Link to="/">integer arithmetic</Link>
                 <Link to="/finite-field">finite field arithmetic</Link>
@@ -42,9 +56,17 @@ const FiniteFieldString = () => {
             </div>
             <div>
                 secret ={' '}
-                <input
+                <textarea
                     value={secret}
                     onChange={(e) => setSecret(e.target.value)}
+                    rows={8}
+                    cols={40}
+                    style={{
+                        backgroundColor: 'rgb(19, 22, 14)',
+                        color: 'white',
+                        fontFamily: 'monospace',
+                        borderRadius: '2px',
+                    }}
                 />
             </div>
             <div>
@@ -81,6 +103,8 @@ const FiniteFieldString = () => {
                             maxWidth: '100%',
                             boxSizing: 'border-box',
                             borderRadius: '4px',
+                            fontFamily: 'monospace',
+                            fontSize: '14px',
                         }}
                     >
                         {share}
@@ -88,7 +112,7 @@ const FiniteFieldString = () => {
                 ))}
             </div>
             <div style={{ fontStyle: 'italic' }}>
-                All the above shares are required to reconstruct the polynomial
+                All the above shares are required to reconstruct the polynomials
                 and the secret
             </div>
             <hr />
@@ -122,7 +146,7 @@ const FiniteFieldString = () => {
                     (_, id) => (
                         <input
                             key={id}
-                            placeholder={`share ${id + 1}`}
+                            placeholder={`Paste the share ${id + 1} here`}
                             value={sharesToReconstruct[id] ?? ''}
                             onChange={(e) =>
                                 setSharesToReconstruct((old) => {
@@ -137,22 +161,59 @@ const FiniteFieldString = () => {
                                 color: 'rgb(197, 208, 245)',
                                 border: '1px solid rgb(26, 26, 61)',
                                 borderRadius: '2px',
+                                fontFamily: 'monospace',
+                                fontSize: '14px',
                             }}
                         />
                     ),
                 )}
             </div>
-            <button onClick={loadShares}>load shares from previous step</button>
-            <button onClick={reconstructSecret}>reconstruct</button>
-            <div>reconstructed secret: </div>
+            <button onClick={loadShares}>Load shares from previous step</button>
+            <button
+                onClick={reconstructSecret}
+                disabled={
+                    sharesToReconstruct.reduce((acc, share) => {
+                        return share.length ? acc + 1 : acc;
+                    }, 0) < numberOfSharesToReconstruct
+                }
+            >
+                Reconstruct
+            </button>
+            <div style={{ marginTop: '16px' }}>Reconstructed secret: </div>
             <div
                 style={{
-                    color: 'lightgreen',
-                    maxWidth: '100%',
+                    maxWidth: 'calc(100% - 16px)',
+                    width: 'max-content',
+                    minWidth: '30ch',
+                    minHeight: '16ch',
                     lineBreak: 'anywhere',
+                    fontSize: '14px',
+                    backgroundColor: 'rgb(19, 22, 14)',
+                    color: 'rgb(186, 211, 146)',
+                    fontFamily: 'monospace',
+                    borderRadius: '2px',
+                    padding: '8px',
+                    border: '1px solid rgb(46, 53, 35)',
+                    margin: '8px',
+                    boxSizing: 'border-box',
                 }}
             >
-                {reconstructedSecret}
+                {error != null ? (
+                    <span style={{ color: 'red', fontStyle: 'italic' }}>
+                        Error: {error}
+                    </span>
+                ) : reconstructedSecret === '' ? (
+                    <span style={{ color: 'gray', fontStyle: 'italic' }}>
+                        Result will appear here after you click Reconstruct.
+                    </span>
+                ) : (
+                    reconstructedSecret.split('\n').map((el, id) => (
+                        <React.Fragment key={id}>
+                            {el}
+                            <br />
+                        </React.Fragment>
+                    ))
+                )}
             </div>
         </div>
     );
